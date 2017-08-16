c1c0edc10c8fbe641c3941ca271d5506d034ca87
Cheng Cheng <ccheng16@outlook.com>
2017-08-16 17:08:42 +0800

    Fixed script

--- feed_kenlm.py
+++ feed_kenlm.py
@@ -3,7 +3,8 @@ import mmap
 
 fpath = "./data/wikipedia/cn_wiki.txt"
 with codecs.open(fpath, 'r', encoding='utf-8') as f:
-    for i in range(10):
-        text = f.readline()
-        print(text)
-        print(' '.join(text))
+    text = f.readlines()
+
+for line in text[:10]:
+    print(' '.join(line.strip()), end=' ')
+    # print(' '.join(line.strip().split(' / ')), end=' ')
